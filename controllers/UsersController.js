a82ad1f1e2b823c8dcb8e177beac21685cc4724f
zyambo <mightypz2@gmail.com>
2022-08-23 03:10:58 -0700

    adding task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,49 @@
+const mongo = require('mongodb');
+const { createHash } = require('crypto');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class UsersController {
+  static async postNew(request, response) {
+    try {
+      const { email, password } = request.body;
+
+      if (!email) {
+        return response.status(400).json({ error: 'Missing email' });
+      }
+
+      if (!password) {
+        return response.status(400).json({ error: 'Missing password' });
+      }
+
+      const collection = dbClient.db.collection('users');
+      const cursor = collection.find({ email });
+      if (await cursor.count() > 0) {
+        return response.status(400).json({ error: 'Already exist' });
+      }
+      const hash = createHash('sha1');
+      hash.update(password);
+      const user = await collection.insertOne({ email, password: hash.digest('hex') });
+
+      return response.status(201).json({ email, id: user.insertedId });
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+  }
+
+  static async getMe(request, response) {
+    const token = request.headers['x-token'];
+    const id = await redisClient.get(`auth_${token}`);
+    const objectId = new mongo.ObjectID(id);
+    const user = await dbClient.db.collection('users').findOne({ _id: objectId });
+
+    if (!user) {
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return response.json({ id, email: user.email });
+  }
+}
+
+module.exports = UsersController;
