2fb67f5f0dec4a82e9a5c005fe3d00319b21ec78
zyambo <mightypz2@gmail.com>
2022-08-23 01:04:42 -0700

    adding task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(request, response) {
+    if (dbClient.isAlive() && redisClient.isAlive()) {
+      response.status(200);
+      response.json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    }
+  }
+
+  static async getStats(request, response) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    response.status(200);
+    response.json({ users, files });
+  }
+}
+
+module.exports = AppController;
